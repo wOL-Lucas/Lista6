9c070788674844c02188444fd0d6b18ba2fa0d4e
wOL-Lucas <lucasefernandes333@gmail.com>
2023-10-17 21:28:47 -0300

    Fix: Changed Exercise number 3 to the correct answer

--- src/main/java/Exercicio3/Exercicio3.java
+++ src/main/java/Exercicio3/Exercicio3.java
@@ -1,37 +1,29 @@
-/*Faça um programa que leia duas matrizes A e B de tamanho 2 x 2 e calcule C = A ∗ B*/
+/*
+Leia duas matrizes 4 x 4 e escreva uma terceira com os maiores valores de cada posição das matrizes
+lidas.
+*/
 package Exercicio3;
+
 import Exercicio1.Exercicio1;
 
 import java.util.Scanner;
 
 public class Exercicio3 {
 
-    public static int[][] multiply_matrix(int[][] matrix_a, int[][] matrix_b){
-        int[][] matrix_c = new int[2][2];
-        for(int i = 0; i < 2; i ++){
-            for(int j = 0; j < 2; j++){
-                for(int k = 0;k < 2;k++){
-                    matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j];
-                }
+
+    public static int[][] create_matrix_with_bigger(int[][]matrix_a, int[][]matrix_b){
+        int[][] matrix_c = new int[4][4];
+        for(int i = 0; i < 4; i++){
+            for(int x = 0; x < 4; x++){
+                matrix_c[i][x] = matrix_a[i][x] > matrix_b[i][x] ? matrix_a[i][x] : matrix_b[i][x];
             }
         }
         return matrix_c;
     }
 
-    public static int[][] get_matrix(Scanner scanner, int number){
-        System.out.println("Enter the matrix number " + number + ":");
-        int[][]matrix = new int[2][2];
-        for(int i = 0; i < 2; i++){
-            for(int x = 0; x < 2; x++){
-                matrix[i][x] = Exercicio1.get_number(scanner);
-            }
-        }
-        return matrix;
-    }
-
     public static void outMatrix(int[][] matrix){
-        for(int i = 0; i < 2; i++){
-            for(int x = 0; x < 2; x++){
+        for(int i = 0; i < 4; i++){
+            for(int x = 0; x < 4; x++){
                 System.out.printf("%d ", matrix[i][x]);
             }
             System.out.println();
@@ -39,11 +31,7 @@ public class Exercicio3 {
     }
 
     public static void main(String[]args){
-        Scanner scanner = new Scanner(System.in);
-        //outMatrix(get_matrix(scanner, 1));
-        //outMatrix(get_matrix(scanner, 2));
-
-        outMatrix(multiply_matrix(get_matrix(scanner,1), get_matrix(scanner, 2)));
-
+        outMatrix(create_matrix_with_bigger(Exercicio1.get_voxcel(new Scanner(System.in)), Exercicio1.get_voxcel(new Scanner(System.in))));
     }
+
 }
--- src/main/java/Exercicio4/Exercicio4.java
+++ src/main/java/Exercicio4/Exercicio4.java
@@ -1,18 +1,17 @@
-/*Faça um programa que leia uma matriz A de tamanho 3 x 3 e calcule B= A2. Onde B é uma nova matriz.*/
-package Exercicio4;
-
-
+/*Faça um programa que leia duas matrizes A e B de tamanho 2 x 2 e calcule C = A ∗ B*/
+package Exercicio3;
 import Exercicio1.Exercicio1;
 
 import java.util.Scanner;
 
-public class Exercicio4 {
-    public static int[][] multiply_matrix(int[][] matrix_a){
-        int[][] matrix_c = new int[3][3];
-        for(int i = 0; i < 3; i ++){
-            for(int j = 0; j < 3; j++){
-                for(int k = 0;k < 3;k++){
-                    matrix_c[i][j] += matrix_a[i][k] * matrix_a[k][j];
+public class Exercicio3 {
+
+    public static int[][] multiply_matrix(int[][] matrix_a, int[][] matrix_b){
+        int[][] matrix_c = new int[2][2];
+        for(int i = 0; i < 2; i ++){
+            for(int j = 0; j < 2; j++){
+                for(int k = 0;k < 2;k++){
+                    matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j];
                 }
             }
         }
@@ -21,9 +20,9 @@ public class Exercicio4 {
 
     public static int[][] get_matrix(Scanner scanner, int number){
         System.out.println("Enter the matrix number " + number + ":");
-        int[][]matrix = new int[3][3];
-        for(int i = 0; i < 3; i++){
-            for(int x = 0; x < 3; x++){
+        int[][]matrix = new int[2][2];
+        for(int i = 0; i < 2; i++){
+            for(int x = 0; x < 2; x++){
                 matrix[i][x] = Exercicio1.get_number(scanner);
             }
         }
@@ -31,8 +30,8 @@ public class Exercicio4 {
     }
 
     public static void outMatrix(int[][] matrix){
-        for(int i = 0; i < 3; i++){
-            for(int x = 0; x < 3; x++){
+        for(int i = 0; i < 2; i++){
+            for(int x = 0; x < 2; x++){
                 System.out.printf("%d ", matrix[i][x]);
             }
             System.out.println();
@@ -40,7 +39,11 @@ public class Exercicio4 {
     }
 
     public static void main(String[]args){
-        outMatrix(multiply_matrix(get_matrix(new Scanner(System.in),1)));
-    }
+        Scanner scanner = new Scanner(System.in);
+        //outMatrix(get_matrix(scanner, 1));
+        //outMatrix(get_matrix(scanner, 2));
 
+        outMatrix(multiply_matrix(get_matrix(scanner,1), get_matrix(scanner, 2)));
+
+    }
 }
